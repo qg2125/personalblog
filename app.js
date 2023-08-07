f2f817f672c1e68875b7c887c13223fccac40623
Cara Qing Ge <geqing2012@hotmail.com>
2023-08-07 12:24:42 -0400

    add projects to home page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import mongoose from 'mongoose';
 
 
-const homeStartingContent = "This journal keeps Cara's notes for learning coding. You can click read more to see the entire notes.";
+const homeStartingContent = "This website demonstrates Cara's projects and notes for learning coding. ";
 const aboutContent = "Don't know what to write...";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
